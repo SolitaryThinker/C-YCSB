5c92a9f75363c968ec739e6f4f5325a75e8d3fbc
William Lin <wlsaidhi@gmail.com>
2018-12-30 23:50:34 -0600

    changed naming

--- src/generator/generator.h
+++ src/generator/generator.h
@@ -6,8 +6,8 @@ namespace cycsb {
 template <typename Value>
 class Generator {
     public:
-        virtual Value Next() = 0;
-        virtual Value Last() = 0;
+        virtual Value NextValue() = 0;
+        virtual Value LastValue() = 0;
         virtual ~Generator() { }
 };
 
