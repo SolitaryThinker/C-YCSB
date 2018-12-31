07b73e7955f75a76f5f5f77dc6f6d6e13e08ff09
William Lin <wlsaidhi@gmail.com>
2018-12-31 00:02:05 -0600

    mostly done with ZipfianGenerator

---
+++ src/generator/zipfian_generator.cc
@@ -0,0 +1,65 @@
+#include "zipfian_generator.h"
+
+#include "utils.h"
+
+namespace cycsb {
+
+uint64_t ZipfianGenerator::NextLong(uint64_t item_count) {
+  if (item_count != count_for_zeta_) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (item_count > count_for_zeta_) {
+      zetan_ = Zeta(count_for_zeta_, item_count, theta_, zetan_);
+      eta_ = (1 - std::pow(2.0 / items_, 1 - theta_)) / (1 - zeta_to_theta_ / zetan_);
+    } else if ((item_count < count_for_zeta_) && (allow_item_count_decrease_)) {
+      //TODO: comments
+      zetan_ = Zeta(item_count, theta_);
+      eta_ = (1 - std::pow(2.0 / items_, 1 - theta_)) / (1 - zeta_to_theta_ / zetan_);
+    }
+  }
+
+  double u = utils::RandomDouble();
+  double uz = u * zetan_;
+
+  if (uz < 1.0) {
+    return base_;
+  }
+
+  if (uz < 1.0 + std::pow(0.5, theta_)) {
+    return base_ + 1;
+  }
+
+  uint64_t ret = base_ + (uint64_t)(item_count * std::pow(eta_ * u - eta_ + 1, alpha_));
+  //setLastValue(ret);
+  return ret;
+}
+
+uint64_t ZipfianGenerator::NextValue() {
+  return NextLong(items_);
+}
+
+double ZipfianGenerator::ZetaStatic(uint64_t n, double theta) {
+  return ZetaStatic(0, n, theta, 0);
+}
+
+double ZipfianGenerator::ZetaStatic(uint64_t st, uint64_t n, double theta,
+                                    double initial_sum) {
+  double sum = initial_sum;
+  for (uint64_t i = st; i < n; ++i) {
+    sum += 1 / (std::pow(i + 1, theta));
+  }
+
+  return sum;
+}
+
+double ZipfianGenerator::Zeta(uint64_t n, double theta) {
+  count_for_zeta_ = n;
+  return ZetaStatic(n, theta);
+}
+
+double ZipfianGenerator::Zeta(uint64_t st, uint64_t n, double theta,
+                              double initial_sum) {
+  count_for_zeta_ = n;
+  return ZetaStatic(st, n, theta, initial_sum);
+}
+
+}  // namespace cycsb
--- src/generator/zipfian_generator.h
+++ src/generator/zipfian_generator.h
@@ -1,7 +1,59 @@
 #ifndef CYCSB_GENERATOR_ZIPFIAN_GENERATOR_H_
 #define CYCSB_GENERATOR_ZIPFIAN_GENERATOR_H_
 
+#include "generator.h"
+
+#include <cstdint>
+#include <cmath>
+#include <mutex>
+
 namespace cycsb {
+
+class ZipfianGenerator : public Generator<uint64_t> {
+  public:
+    static constexpr double const kZipfianConst = 0.99;
+
+    ZipfianGenerator(uint64_t items) : ZipfianGenerator(0, items - 1) {
+    }
+
+    ZipfianGenerator(uint64_t min, uint64_t max)
+      : ZipfianGenerator(min, max, kZipfianConst) {
+    }
+
+    ZipfianGenerator(uint64_t min, uint64_t max, double zipfian_const)
+      : ZipfianGenerator(min, max, kZipfianConst,
+                         ZetaStatic(max - min + 1, zipfian_const)) {
+    }
+
+    ZipfianGenerator(uint64_t min, uint64_t max, double zipfian_const,
+                     double zetan)
+      : items_(max - min + 1),
+        base_(min),
+        zipfian_const_(zipfian_const),
+        theta_(zipfian_const),
+        zeta_to_theta_(Zeta(2, theta_)),
+        alpha_(1.0 / (1.0 - theta_)),
+        zetan_(zetan),
+        count_for_zeta_(items_),
+        eta_((1 - std::pow(2.0 / items_, 1 - theta_))
+             / (1 - zeta_to_theta_ / zetan_)) {
+    }
+
+    uint64_t NextLong(uint64_t item_count);
+    uint64_t NextValue();
+
+   private:
+        static double ZetaStatic(uint64_t n, double theta);
+        static double ZetaStatic(uint64_t st, uint64_t n, double theta,
+                                 double initial_sum);
+        double Zeta(uint64_t n, double theta);
+        double Zeta(uint64_t st, uint64_t n, double theta, double initial_sum);
+
+    uint64_t items_, base_, count_for_zeta_;
+    double zipfian_const_, theta_, zeta_to_theta_, alpha_, zetan_, eta_;
+    std::mutex mutex_;
+    bool allow_item_count_decrease_ = false;
+};
 
 }  // cycsb
 
