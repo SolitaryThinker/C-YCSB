abd858ec477ceef9e399e8defb744920e1ba4ce7
William Lin <wlsaidhi@gmail.com>
2019-01-07 19:20:55 -0600

    added DiscreteGenerator

---
+++ src/generator/discrete_generator.cc
@@ -0,0 +1,39 @@
+//
+//  discrete_generator.cc
+//  YCSB-C
+//
+//  Created by Jinglei Ren on 12/6/14.
+//  Modified by William Lin
+//  Copyright (c) 2014 Jinglei Ren <jinglei@ren.systems>.
+//
+
+#include "generator/discrete_generator.h"
+
+#include "util/utils.h"
+#include <cassert>
+
+namespace cycsb {
+
+template <typename T>
+void DiscreteGenerator<T>::AddValue(T value, double weight) {
+  values_.push_back(std::make_pair(value, weight));
+  sum_ += weight;
+}
+
+template <typename T>
+T DiscreteGenerator<T>::NextValue() {
+  mutex_.lock();
+  double chooser = utils::RandomDouble();
+  mutex_.unlock();
+
+  for (auto p = values_.cbegin(); p != values_.cend(); ++p) {
+    if (chooser < p->second / sum_) {
+      return last_ = p->first;
+    }
+    chooser -= p->second / sum_;
+  }
+
+  assert(false);
+}
+
+}  // namespace cycsb
---
+++ src/generator/discrete_generator.h
@@ -0,0 +1,44 @@
+//
+//  discrete_generator.h
+//  YCSB-C
+//
+//  Created by Jinglei Ren on 12/6/14.
+//  Modified by William Lin
+//  Copyright (c) 2014 Jinglei Ren <jinglei@ren.systems>.
+//
+
+#ifndef CYCSB_GENERATOR_DISCRETE_GENERATOR_H_
+#define CYCSB_GENERATOR_DISCRETE_GENERATOR_H_
+
+#include "generator/generator.h"
+#include <atomic>
+#include <mutex>
+#include <string>
+#include <vector>
+
+namespace cycsb {
+
+template <typename T>
+class DiscreteGenerator : public Generator<T> {
+  public:
+    DiscreteGenerator() : sum_(0) { }
+    void AddValue(T value, double weight);
+
+    T NextValue();
+    T LastValue() {
+      if (values_.empty()) {
+        last_ = NextValue();
+      }
+      return last_;
+    }
+
+  protected:
+    std::vector<std::pair<T, double>> values_;
+    double sum_;
+    std::atomic<T> last_;
+    std::mutex mutex_;
+};
+
+}  // namespace cycsb
+
+#endif  // CYCSB_GENERATOR_DISCRETE_GENERATOR_H_
